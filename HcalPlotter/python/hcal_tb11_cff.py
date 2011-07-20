ea7636a37848791af52c14a3d0675c13ad945b83
andersj <>
2011-07-20 16:08:51 +0000

    new maps

--- HcalPlotter/python/hcal_tb11_cff.py
+++ HcalPlotter/python/hcal_tb11_cff.py
@@ -7,13 +7,21 @@ hcal_db_producer = cms.ESProducer("HcalDbProducer",
  
 hcales_ascii = cms.ESSource("HcalTextCalibrations",
                         input = cms.VPSet(
+    ## cms.PSet(
+    ## object = cms.string('ElectronicsMap'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HBHO_HPD.txt')
+    ## ),
     cms.PSet(
     object = cms.string('ElectronicsMap'),
-    file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HBHO_HPD.txt')
+    file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HB_HPD_HO_SiPM.txt')
     ),
+    ## cms.PSet(
+    ## object = cms.string('Pedestals'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HBHO_HPD.txt')
+    ## ),
     cms.PSet(
     object = cms.string('Pedestals'),
-    file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HBHO_HPD.txt')
+    file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HB_HPD_HO_SiPM.txt')
     ),
     cms.PSet(
     object = cms.string('Gains'),
