11268933026ad2180c6e0a3b25af06f617387d4e
andersj <>
2011-07-26 07:28:13 +0000

    new for HB SiPMs

--- HcalPlotter/python/hcal_tb11_cff.py
+++ HcalPlotter/python/hcal_tb11_cff.py
@@ -7,34 +7,46 @@ hcal_db_producer = cms.ESProducer("HcalDbProducer",
  
 hcales_ascii = cms.ESSource("HcalTextCalibrations",
                         input = cms.VPSet(
+    ## cms.PSet(
+    ## object = cms.string('ElectronicsMap'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HBHO_HPD.txt')
+    ## ),
+    ## cms.PSet(
+    ## object = cms.string('ElectronicsMap'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HB_HPD_HO_SiPM.txt')
+    ## ),
     cms.PSet(
     object = cms.string('ElectronicsMap'),
-    file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HBHO_HPD.txt')
+    file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HB_SiPM_HO_SiPM_2.txt')
     ),
-##     cms.PSet(
-##     object = cms.string('ElectronicsMap'),
-##     file = cms.FileInPath('andersj/HcalPlotter/data/tb2011_map_HB_HPD_HO_SiPM.txt')
-##     ),
+    ## cms.PSet(
+    ## object = cms.string('Pedestals'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HBHO_HPD.txt')
+    ## ),
+    ## cms.PSet(
+    ## object = cms.string('Pedestals'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HB_HPD_HO_SiPM.txt')
+    ## ),
     cms.PSet(
     object = cms.string('Pedestals'),
-    file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HBHO_HPD.txt')
+    file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HB_SiPM_HO_SiPM_2.txt')
     ),
-##     cms.PSet(
-##     object = cms.string('Pedestals'),
-##     file = cms.FileInPath('andersj/HcalPlotter/data/ped_tb2011_HB_HPD_HO_SiPM.txt')
-##     ),
+    ## cms.PSet(
+    ## object = cms.string('Gains'),
+    ## file = cms.FileInPath('andersj/HcalPlotter/data/gain_tb2011_HBHO_HPD_1.txt')
+    ## )
     cms.PSet(
     object = cms.string('Gains'),
-    file = cms.FileInPath('andersj/HcalPlotter/data/gain_tb2011_HBHO_HPD_1.txt')
+    file = cms.FileInPath('andersj/HcalPlotter/data/gain_tb2011_HB_SiPM_HO_SiPM_2.txt')
     )
-    )
+                                )
                             )
 
 hcales_hardcode = cms.ESSource("HcalHardcodeCalibrations",
                                toGet = cms.untracked.vstring(
-##                                                              'Pedestals',
+                                                             # 'Pedestals',
                                                              'PedestalWidths', 'LutMetadata',
-##                                                              'Gains',
+                                                             # 'Gains',
                                                              'GainWidths', 'LUTCorrs', 
                                                              'PFCorrs', 'QIEData',
                                                              'L1TriggerObjects','ZSThresholds','DcsValues',
